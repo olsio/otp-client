91440e5d498ed617dfee1d6827840d9e5c15527b
Oliver Schneider <os@ols.io>
2019-02-26 10:22:32 +0100

    Version Upgrades

--- src/crypto_utils.js
+++ src/crypto_utils.js
@@ -1,6 +1,6 @@
-import base32 from "thirty-two"
-import crypto from "crypto"
-import leftPad from "left-pad"
+import base32 from 'thirty-two'
+import crypto from 'crypto'
+import leftPad from 'left-pad'
 
 const hexToInt = hex => {
   return parseInt(hex, 16)
@@ -9,28 +9,31 @@ const hexToInt = hex => {
 const prepareCounter = counter => {
   const hexCounter = parseInt(counter, 10).toString(16)
   const paddedCounter = leftPad(hexCounter, 16, 0)
-  return new Buffer(paddedCounter, "hex")
+  return Buffer.from(paddedCounter, 'hex')
 }
 
 const toBase32 = value => {
-  return base32.encode(value).toString().replace(/=/g, "")
+  return base32
+    .encode(value)
+    .toString()
+    .replace(/=/g, '')
 }
 
 const prepareSecret = (secret, algorithm) => {
-  const hexSecret = base32.decode(secret).toString("hex")
+  const hexSecret = base32.decode(secret).toString('hex')
   const paddedSecret = padSecret(hexSecret, algorithm)
   return paddedSecret
 }
 
 const padSecret = (secret, algorithm) => {
-  const encoded = new Buffer(secret, "hex")
+  const encoded = Buffer.from(secret, 'hex')
 
   switch (algorithm) {
-    case "sha1":
+    case 'sha1':
       return padBuffer(encoded, 20)
-    case "sha256":
+    case 'sha256':
       return padBuffer(encoded, 32)
-    case "sha512":
+    case 'sha512':
       return padBuffer(encoded, 64)
     default:
       return encoded
@@ -41,7 +44,7 @@ const padBuffer = (secretBuffer, size) => {
   while (secretBuffer.length < size && secretBuffer.length != 0) {
     secretBuffer = Buffer.concat([secretBuffer, secretBuffer])
   }
-  return new Buffer(secretBuffer, "hex").slice(0, size)
+  return Buffer.from(secretBuffer, 'hex').slice(0, size)
 }
 
 const generateToken = (counter, algorithm, secret, digits) => {
@@ -49,10 +52,10 @@ const generateToken = (counter, algorithm, secret, digits) => {
     algorithm,
     prepareSecret(secret, algorithm)
   )
-  const hmac = cryptoHmac.update(prepareCounter(counter)).digest("hex")
+  const hmac = cryptoHmac.update(prepareCounter(counter)).digest('hex')
   const offset = hexToInt(hmac.substr(hmac.length - 1))
   const truncatedHash = hmac.substr(offset * 2, 8)
-  const sigbit0 = hexToInt(truncatedHash) & hexToInt("7fffffff")
+  const sigbit0 = hexToInt(truncatedHash) & hexToInt('7fffffff')
   const token = sigbit0 % Math.pow(10, digits)
   return leftPad(token, digits, 0)
 }
--- src/reference-validation/rfc6238.test.js
+++ src/reference-validation/rfc6238.test.js
@@ -1,120 +1,120 @@
-import OTP from ".."
-import { toBase32 } from "../crypto_utils"
+import OTP from '..'
+import { toBase32 } from '../crypto_utils'
 
-const secret = "12345678901234567890"
+const secret = '12345678901234567890'
 
 const table = [
   {
     epoch: 59,
-    counter: "0000000000000001",
-    token: "94287082",
-    algorithm: "SHA1"
+    counter: '0000000000000001',
+    token: '94287082',
+    algorithm: 'SHA1'
   },
   {
     epoch: 59,
-    counter: "0000000000000001",
-    token: "46119246",
-    algorithm: "SHA256"
+    counter: '0000000000000001',
+    token: '46119246',
+    algorithm: 'SHA256'
   },
   {
     epoch: 59,
-    counter: "0000000000000001",
-    token: "90693936",
-    algorithm: "SHA512"
+    counter: '0000000000000001',
+    token: '90693936',
+    algorithm: 'SHA512'
   },
   {
     epoch: 1111111109,
-    counter: "00000000023523EC",
-    token: "07081804",
-    algorithm: "SHA1"
+    counter: '00000000023523EC',
+    token: '07081804',
+    algorithm: 'SHA1'
   },
   {
     epoch: 1111111109,
-    counter: "00000000023523EC",
-    token: "68084774",
-    algorithm: "SHA256"
+    counter: '00000000023523EC',
+    token: '68084774',
+    algorithm: 'SHA256'
   },
   {
     epoch: 1111111109,
-    counter: "00000000023523EC",
-    token: "25091201",
-    algorithm: "SHA512"
+    counter: '00000000023523EC',
+    token: '25091201',
+    algorithm: 'SHA512'
   },
   {
     epoch: 1111111111,
-    counter: "00000000023523ED",
-    token: "14050471",
-    algorithm: "SHA1"
+    counter: '00000000023523ED',
+    token: '14050471',
+    algorithm: 'SHA1'
   },
   {
     epoch: 1111111111,
-    counter: "00000000023523ED",
-    token: "67062674",
-    algorithm: "SHA256"
+    counter: '00000000023523ED',
+    token: '67062674',
+    algorithm: 'SHA256'
   },
   {
     epoch: 1111111111,
-    counter: "00000000023523ED",
-    token: "99943326",
-    algorithm: "SHA512"
+    counter: '00000000023523ED',
+    token: '99943326',
+    algorithm: 'SHA512'
   },
   {
     epoch: 1234567890,
-    counter: "000000000273EF07",
-    token: "89005924",
-    algorithm: "SHA1"
+    counter: '000000000273EF07',
+    token: '89005924',
+    algorithm: 'SHA1'
   },
   {
     epoch: 1234567890,
-    counter: "000000000273EF07",
-    token: "91819424",
-    algorithm: "SHA256"
+    counter: '000000000273EF07',
+    token: '91819424',
+    algorithm: 'SHA256'
   },
   {
     epoch: 1234567890,
-    counter: "000000000273EF07",
-    token: "93441116",
-    algorithm: "SHA512"
+    counter: '000000000273EF07',
+    token: '93441116',
+    algorithm: 'SHA512'
   },
   {
     epoch: 2000000000,
-    counter: "0000000003F940AA",
-    token: "69279037",
-    algorithm: "SHA1"
+    counter: '0000000003F940AA',
+    token: '69279037',
+    algorithm: 'SHA1'
   },
   {
     epoch: 2000000000,
-    counter: "0000000003F940AA",
-    token: "90698825",
-    algorithm: "SHA256"
+    counter: '0000000003F940AA',
+    token: '90698825',
+    algorithm: 'SHA256'
   },
   {
     epoch: 2000000000,
-    counter: "0000000003F940AA",
-    token: "38618901",
-    algorithm: "SHA512"
+    counter: '0000000003F940AA',
+    token: '38618901',
+    algorithm: 'SHA512'
   },
   {
     epoch: 20000000000,
-    counter: "0000000027BC86AA",
-    token: "65353130",
-    algorithm: "SHA1"
+    counter: '0000000027BC86AA',
+    token: '65353130',
+    algorithm: 'SHA1'
   },
   {
     epoch: 20000000000,
-    counter: "0000000027BC86AA",
-    token: "77737706",
-    algorithm: "SHA256"
+    counter: '0000000027BC86AA',
+    token: '77737706',
+    algorithm: 'SHA256'
   },
   {
     epoch: 20000000000,
-    counter: "0000000027BC86AA",
-    token: "47863826",
-    algorithm: "SHA512"
+    counter: '0000000027BC86AA',
+    token: '47863826',
+    algorithm: 'SHA512'
   }
 ]
 
-describe("RFC 6238", () => {
+describe('RFC 6238', () => {
   table.forEach(entry => {
     test(`[${entry.epoch} + ${entry.algorithm}] => ${entry.token}`, () => {
       const otp = new OTP(toBase32(secret), {
