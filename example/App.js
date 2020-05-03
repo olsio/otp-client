7f392b8e4cb9ac9b7f1925c9537f4d5b8f599f7b
Oliver Schneider <os@ols.io>
2020-05-03 14:08:26 +0200

    Draft

--- example/App.js
+++ example/App.js
@@ -1,27 +1,98 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import OTP from 'otp-client'
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default class App extends React.Component {
-  render() {
-    const secret = 'TPQDAHVBZ5NBO5LFEQKC7V7UPATSSMFY'
-    const otp = new OTP(secret)
-    const token = otp.getToken()
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-    return (
-      <View style={styles.container}>
-        <Text />
-        <Text>{token}</Text>
-      </View>
-    )
-  }
-}
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import OTP from 'otp-client';
+
+const App: () => React$Node = () => {
+  const secret = 'TPQDAHVBZ5NBO5LFEQKC7V7UPATSSMFY';
+  const otp = new OTP(secret);
+  const token = otp.getToken();
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Token</Text>
+              <Text>{token}</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
